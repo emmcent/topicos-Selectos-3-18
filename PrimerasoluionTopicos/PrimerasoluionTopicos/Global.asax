9f2394db214ae870e9a45fd6f6b74a8fe98632ed
laboratorio <laboratorio@SP-LAB9-08>
2018-09-22 15:34:25 -0600

    Primera versión de la aplicación

---
+++ PrimerasoluionTopicos/PrimerasoluionTopicos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrimerasoluionTopicos.MvcApplication" Language="C#" %>
---
+++ PrimerasoluionTopicos/PrimerasoluionTopicos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrimerasoluionTopicos.Startup))]
+namespace PrimerasoluionTopicos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
